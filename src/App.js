ec126cbc76160541254f71803a3724559d7c620e
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2018-04-27 23:05:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './sass/app.css'
+import Row from './components/row.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default App;
